9e8daa2ddae8ce8e4299c2618ce432cc04f0abf6
Jakub Kalina <jakub.kalina@allegro.pl>
2015-06-08 12:06:37 +0200

    added src flag

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,18 @@
 
 var gulp = require('gulp'),
     wrench = require('wrench'),
+    util = require('gulp-util'),
     options = require('./config.json'),
-    secret = require('./secret.json')
+    secret = require('./secret.json');
 
 wrench.readdirSyncRecursive('./gulp').filter(function(file) {
     return (/\.(js|coffee)$/i).test(file);
 }).map(function(file) {
+
+    if (util.env.src) {
+        options.paths.src = util.env.src; 
+    }
+    
     require('./gulp/' + file)(options, secret);
 });
 
